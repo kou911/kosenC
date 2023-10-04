4381bfb511972b63ac89664278a8c23af5f9d5b4
kou <122082307+kou911@users.noreply.github.com>
2023-10-04 15:30:26 +0900

    first commit

---
+++ mark.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mark : MonoBehaviour
+{
+
+    public SpriteRenderer go;
+    public SpriteRenderer title;
+    public float rnd;
+    public float waittime;
+    public float score;
+    public GameObject score_object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Text scoretext = score_object.GetComponent<Text> ();
+
+        if(Input.GetKey(KeyCode.A) && title.enabled){
+            title.enabled = false;
+            rnd = Random.Range(2.00f, 5.00f);
+        }
+
+        if(!title.enabled){
+            waittime += Time.deltaTime;
+        }
+
+        if(waittime >= rnd){
+            go.enabled = true;
+        }
+
+        if(Input.GetKey(KeyCode.S) && !title.enabled){
+            title.enabled = true;
+
+            if(go.enabled){
+                score = waittime - rnd;
+                scoretext.text = score.ToString() + "秒";
+            }
+
+            else{
+                scoretext.text = "失敗";
+            }
+
+            waittime = 0;
+            go.enabled = false;
+        }
+    }
+}
